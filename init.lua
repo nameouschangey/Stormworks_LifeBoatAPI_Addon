b450c3b2709528cf6d4a8cc9e115c17c994164da
nameouschangey@github.com <722981+nameouschangey@users.noreply.github.com>
2022-06-14 15:34:31 +0100

    Create init.lua
    Added init.lua to fix require issue

---
+++ init.lua
@@ -0,0 +1,14 @@
+---@section LIFEBOATAPI_MC_REQUIREALL_BOILERPLATE
+-- Author: Nameous Changey
+-- GitHub: https://github.com/nameouschangey/STORMWORKS_VSCodeExtension
+-- Workshop: https://steamcommunity.com/id/Bilkokuya/myworkshopfiles/?appid=573090
+--- Developed using LifeBoatAPI - Stormworks Lua plugin for VSCode - https://code.visualstudio.com/download (search "Stormworks Lua with LifeboatAPI" extension)
+--- If you have any issues, please report them here: https://github.com/nameouschangey/STORMWORKS_VSCodeExtension/issues - by Nameous Changey
+--- "Require All" file to make life easier on the end-user
+---@endsection
+
+
+-- The addon library is currently in development
+-- If you keep this in your project, it will automatically update itself when released.
+
+-- Thank you for your patience
